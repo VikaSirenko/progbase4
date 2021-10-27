357e486425b293e8efdd2228b4f09acf64d804f4
Vika Sirenko <sirenko.victoria.kp03@gmail.com>
2021-10-27 09:32:22 +0300

    lab 2 done

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -6,8 +6,51 @@ class Program
     static void Main(string[] args)
     {
 
+        //1
+        try
+        {
+            JuniorCard juniorCard = new JuniorCard(null, 12, "1234");
+        }
+        catch (ProgramExeption ex)
+        {
+            Console.WriteLine(ex.Message + "  " + ex.exceptionArgs.ErrorTime);
+        }
+        //You must enter your username
 
 
+        //2
+        try
+        {
+            PensionCard pensionCard = new PensionCard("Maria", 26);
+        }
+        catch (ProgramExeption ex)
+        {
+            Console.WriteLine(ex.Message + "  " + ex.exceptionArgs.ErrorTime);
+
+        }
+
+        //You do not fit the age of the Pension Card, choose another card
+
+
+
+
+        //3 
+        try
+        {
+            JuniorCard juniorCard1 = new JuniorCard("Polina", 17, "1234");
+            juniorCard1.PutMoney(50500);
+        }
+        catch (ProgramExeption ex)
+        {
+            Console.WriteLine(ex.Message + "  " + ex.exceptionArgs.ErrorTime);
+        }
+        //It is not possible to put money on the card, because the card has a limit of 20,000
+
+
+
+        //4
+        JuniorCard juniorCard2 = new JuniorCard("Polina", 17, "1234");
+        juniorCard2.RegisterForBankTour();
     }
 }
 
@@ -49,7 +92,8 @@ public abstract class BankCard
             }
             else
             {
-                Console.WriteLine("You must enter your username");
+                ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "You must enter your username", LocationOfError = nameof(BankCard), ErrorTime = DateTime.Now };
+                throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
 
             }
 
@@ -113,7 +157,8 @@ public class JuniorCard : BankCard
             }
             else
             {
-                Console.WriteLine("You do not fit the age of the Junior Card, choose another card");
+                ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "You do not fit the age of the Junior Card, choose another card", LocationOfError = nameof(JuniorCard), ErrorTime = DateTime.Now };
+                throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
             }
         }
     }
@@ -122,7 +167,8 @@ public class JuniorCard : BankCard
     {
         if (args.amount_of_money < 0 || args.amount_of_money > amount_of_money)
         {
-            Console.WriteLine("It is impossible to withdraw money");
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "It is impossible to withdraw money", LocationOfError = nameof(JuniorCard), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
         }
         else if (args.amount_of_money >= 3000)
         {
@@ -145,8 +191,8 @@ public class JuniorCard : BankCard
         }
         else if (amount_of_money + amount > 20000)
         {
-            Console.WriteLine("It is not possible to put money on the card, because the card has a limit of 20,000");
-
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "It is not possible to put money on the card, because the card has a limit of 20,000", LocationOfError = nameof(JuniorCard), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
         }
         else
         {
@@ -191,7 +237,8 @@ public class PensionCard : BankCard, ISpecialOpportunities
             }
             else
             {
-                Console.WriteLine("You do not fit the age of the Pension Card, choose another card");
+                ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "You do not fit the age of the Pension Card, choose another card", LocationOfError = nameof(PensionCard), ErrorTime = DateTime.Now };
+                throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
             }
         }
     }
@@ -214,7 +261,8 @@ public class PensionCard : BankCard, ISpecialOpportunities
         }
         else if (amount_of_money + amount > 100000)
         {
-            Console.WriteLine("It is not possible to put money on the card, because the card has a limit of 100,000");
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "It is not possible to put money on the card, because the card has a limit of 100,000", LocationOfError = nameof(PensionCard), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
 
         }
         else
@@ -228,7 +276,8 @@ public class PensionCard : BankCard, ISpecialOpportunities
     {
         if (args.amount_of_money < 0 || args.amount_of_money > amount_of_money)
         {
-            Console.WriteLine("It is impossible to withdraw money");
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "It is impossible to withdraw money", LocationOfError = nameof(PensionCard), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
         }
         else
         {
@@ -289,7 +338,9 @@ public class VipCard : BankCard, ISpecialOpportunities, IVipService
             }
             else
             {
-                Console.WriteLine("You do not fit the age of the Vip Card, choose another card");
+                ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "You do not fit the age of the Vip Card, choose another card", LocationOfError = nameof(VipCard), ErrorTime = DateTime.Now };
+                throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
+
             }
         }
     }
@@ -308,7 +359,8 @@ public class VipCard : BankCard, ISpecialOpportunities, IVipService
     {
         if (amount < 0)
         {
-            Console.WriteLine("Set value is negative");
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "Set value is negative", LocationOfError = nameof(VipCard), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
         }
         else
         {
@@ -326,7 +378,8 @@ public class VipCard : BankCard, ISpecialOpportunities, IVipService
     {
         if (args.amount_of_money < 0 || args.amount_of_money > amount_of_money)
         {
-            Console.WriteLine("It is impossible to withdraw money");
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "It is impossible to withdraw money", LocationOfError = nameof(VipCard), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
         }
         else if (args.amount_of_money > 100000)
         {
@@ -409,6 +462,10 @@ public class User
     public string name;
     public int age;
     public event WithdrawMoneyHandle WithdrawMoneyEvent;
+
+    public event Action<User, WithdrawMoneyEventArgs> WithdrawMoneyEvent2;
+
+    public event Func<double> CheckBalanceEvent;
     public User(string name, int age)
     {
         this.name = name;
@@ -453,20 +510,146 @@ public class WithdrawMoneyEventArgs : EventArgs
 
 public delegate void WithdrawMoneyHandle(User user, WithdrawMoneyEventArgs args);
 
+public delegate double CheckBalanceHandle(User user);
+
 class Bank
 {
-    BankCard[] cards;
+    private BankCard card;
+    private double amount_of_money;
+
     public Bank(User user)
     {
-        cards = new BankCard[3];
-        cards[0] = new JuniorCard();
-        cards[1] = new PensionCard();
-        cards[2] = new VipCard();
+        this.DetermineTypeOfCard();
+        this.amount_of_money = card.CheckBalance();
 
-        foreach (BankCard card in cards)
+        user.WithdrawMoneyEvent += new WithdrawMoneyHandle(this.card.WithdrawMoney);
+
+
+
+        //anonymous method
+        user.WithdrawMoneyEvent += delegate (User user, WithdrawMoneyEventArgs args)
+         {
+             this.WithdrawMoney(user, args);
+
+         };
+
+
+
+        // lambda expression
+        user.WithdrawMoneyEvent += (User user, WithdrawMoneyEventArgs args) => this.WithdrawMoney(user, args);
+
+
+        //Action 
+        user.WithdrawMoneyEvent2 += new Action<User, WithdrawMoneyEventArgs>(card.WithdrawMoney);
+
+
+        //Func
+        user.CheckBalanceEvent += card.CheckBalance;
+
+    }
+
+    private void WithdrawMoney(User user, WithdrawMoneyEventArgs args)
+    {
+        if (args.amount_of_money < 0 || args.amount_of_money > card.CheckBalance())
         {
-            user.WithdrawMoneyEvent += new WithdrawMoneyHandle(card.WithdrawMoney);
+            ProgramExeptionArgs errArgs = new ProgramExeptionArgs { ErrorMessage = "It is impossible to withdraw money", LocationOfError = nameof(Bank), ErrorTime = DateTime.Now };
+            throw new ProgramExeption(errArgs.ErrorMessage, errArgs);
+        }
+        else
+        {
+            this.amount_of_money = card.CheckBalance() - args.amount_of_money;
+
         }
 
+    }
+
+    private void DetermineTypeOfCard()
+    {
+        Console.WriteLine("What type of card do you have?");
+        string cardType = Console.ReadLine();
+        switch (cardType)
+        {
+            case "vip":
+                this.card = new VipCard();
+                break;
+            case "junior":
+                this.card = new JuniorCard();
+                break;
+            case "pension":
+                this.card = new PensionCard();
+                break;
+            default:
+                ProgramExeptionArgs args = new ProgramExeptionArgs { ErrorMessage = "Such a card does not exist", LocationOfError = nameof(DetermineTypeOfCard), ErrorTime = DateTime.Now };
+                throw new ProgramExeption(args.ErrorMessage, args);
+
+        }
+
+    }
+
+
+
+}
+
+class ProgramExeption : Exception
+{
+    public ProgramExeptionArgs exceptionArgs;
+    public ProgramExeption(string message, ProgramExeptionArgs args) : base(message)
+    {
+        this.exceptionArgs = args;
+    }
+
+}
+
+class ProgramExeptionArgs
+{
+    private string errorMessage;
+    private string locationOfError;
+    private DateTime errorTime;
+
+    public string ErrorMessage
+    {
+        get
+        {
+            return this.errorMessage;
+        }
+        set
+        {
+            this.errorMessage = value;
+        }
+    }
+
+    public string LocationOfError
+    {
+        get
+        {
+            return this.locationOfError;
+        }
+        set
+        {
+            this.locationOfError = value;
+        }
+    }
+
+    public DateTime ErrorTime
+    {
+        get
+        {
+            return this.errorTime;
+        }
+        set
+        {
+            this.errorTime = value;
+        }
+    }
+
+}
+
+
+
+static class JuniorCardExtension
+{
+    public static void RegisterForBankTour(this JuniorCard juniorCard)
+    {
+        Console.WriteLine("[{0}] --- you have registered for a tour of the bank. We look forward to seeing you!", juniorCard.Name);
     }
 }
